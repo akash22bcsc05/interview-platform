7c77ac609b54c5a331289811aa9866e911e8a625
akash22bcsc05 <cse.22bcsc05@silicon.ac.in>
2026-03-06 22:24:52 +0530

    Updated files before connecting firebase

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import { getRandomInterviewCover } from "@/lib/utils";
 import dayjs from "dayjs";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import DisplayTechIcons from "./DisplayTechIcons";
 
 const InterviewCard = ({ interviewId, userId, role, type, techstack, createdAt }: InterviewCardProps) => {
     const feedback = null as Feedback | null;
@@ -29,9 +32,22 @@ const InterviewCard = ({ interviewId, userId, role, type, techstack, createdAt }
                             </div>
                             
                             <div className="flex flex-row gap-2 items-center">
-
+                                <Image src="/star.svg" alt="star" width={22} height={22} />
+                                <p>{feedback?.totalScore || '---'}/100</p>
                             </div>
                         </div>
+                        <p className="line-clamp-2 mt-5">
+                            {feedback?.finalAssessment || "You haven't taken the interview yet. Take it now to improve your skills."}
+                        </p>
+                    </div>
+
+                    <div className="flex flex-row justify-between">
+                        <DisplayTechIcons techStack={techstack} />
+                        <Button className="btn-primary">
+                            <Link href={feedback ? `/interview/${interviewId}/feedback` : `/interview/${interviewId}`}>
+                            {feedback ? "Check Feedback" : "View Interview"}
+                                </Link>
+                        </Button>
                     </div>
                 </div>
             </div>
