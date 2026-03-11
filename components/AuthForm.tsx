10c6cf0ea44c63798d6e753a227070d9c70aee88
Ayushman Pani <126395065+ayushmanpani@users.noreply.github.com>
2026-03-11 16:53:23 +0530

    Setup Firebase authentication, Firestore database integration, and connect signup/signin flows

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -12,6 +12,9 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { toast } from 'sonner'
 import FormField from './FormField'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '@/firebase/client'
+import { signIn, signUp } from '@/lib/actions/auth.action'
 
 const authFormSchema = (type: FormType) => {
     return z.object({
@@ -34,13 +37,44 @@ const AuthForm = ({ type }: { type: FormType }) => {
         },
     })
 
-    function onSubmit(values: z.infer<typeof formSchema>) {
+    async function onSubmit(values: z.infer<typeof formSchema>) {
         try {
             if (type === "sign-up") {
+                const { name,email,password } = values;
+
+                const UserCredentials  = await createUserWithEmailAndPassword(auth,email,password);
+
+                const result  = await signUp({
+                    uid : UserCredentials.user.uid,
+                    name : name!,
+                    email,
+                    password,
+                })
+
+                if(!result?.success){
+                    toast.error(result?.message);
+                    return
+                }
+
+
                 toast.success('Account created successfully!');
                 router.push('/sign-in');
             }
             else {
+                const {email,password} = values;
+
+                const userCredential = await signInWithEmailAndPassword(auth,email,password);
+
+                const idToken = await userCredential.user.getIdToken();
+
+                if(!idToken){
+                    toast.error('Sign In failed.');
+                    return;
+                }
+
+                await signIn({
+                    email,idToken
+                })
                 toast.success('Signed in successfully!');
                 router.push('/');
             }
@@ -53,7 +87,7 @@ const AuthForm = ({ type }: { type: FormType }) => {
     const isSignIn = type === "sign-in";
 
     return (
-        <div className='card-border lg:min-w-[566px]'>
+        <div className='card-border lg:min-w-141.5'>
             <div className="flex flex-col gap-6 card py-14 px-10">
                 <div className="flex flex-row gap-2 justify-center">
                     <Image src="/logo.svg" alt="logo" height={32} width={38} />
