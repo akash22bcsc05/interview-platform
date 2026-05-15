d08cae912d5a4c8484227a99e519277b693e1a18
akash22bcsc05 <cse.22bcsc05@silicon.ac.in>
2026-05-15 12:30:55 +0530

    deployable1

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -103,7 +103,11 @@ const Agent = ({ userName, userId, type, interviewId, questions }: AgentProps) =
     setCallStatus(CallStatus.CONNECTING);
 
     if(type === "generate") {
-        await vapi.start(process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!,{
+        await vapi.start(
+            undefined,
+            undefined,
+            undefined,
+            process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!,{
         variableValues:{
             username: userName,
             userid: userId,
